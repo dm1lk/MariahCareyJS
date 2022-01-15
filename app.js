5a5b01153f4aca503d90eb742f22812e110557ff
dm1lk <77703500+dm1lk@users.noreply.github.com>
2022-01-15 21:41:42 +0000

    Amend small presence check error.

--- app.js
+++ app.js
@@ -34,7 +34,9 @@ client.once(`ready`, async () => {
 client.on(`interactionCreate`, async (interaction) => {
   if (!interaction.guild) return interaction.reply({embeds: [{description: `🛑 Unforunately, I cannot execute commands outside of a guild.`, color: `ff0000 `}]});
   const user = interaction.options.getUser(`member`);
-  const member = await interaction.guild.members.fetch(user.id || interaction.user.id);
+  let member;
+  if (!user) member = await interaction.guild.members.fetch(interaction.user.id);
+  else member = await interaction.guild.members.fetch(user.id);
   const loop = interaction.options.getBoolean(`loop`) || false;
   if (interaction.commandName == `play`) {
     if (member.voice.channel == null) {
@@ -44,24 +46,30 @@ client.on(`interactionCreate`, async (interaction) => {
       return interaction.reply({embeds: [{description: `🛑 Please join a voice channel first!`, color: `ff0000 `}], ephemeral: true});
     }
     try {
+      console.log(`[GUILD/${interaction.guild.id}] Connecting to VC "${member.voice.channel.name}"...`);
       connection = await joinVoiceChannel({
         channelId: member.voice.channel.id,
         guildId: interaction.guild.id,
         adapterCreator: interaction.guild.voiceAdapterCreator,
       });
+      console.log(`[GUILD/${interaction.guild.id}] Connected to VC "${member.voice.channel.name}"!`);
       connection.on(VoiceConnectionStatus.Disconnected, async (oldState, newState) => {
+        console.log(`[GUILD/${interaction.guild.id}] Lost connection to VC "${member.voice.channel.name}", attempting to reconnect...`);
         try {
           await Promise.race([
             entersState(connection, VoiceConnectionStatus.Signalling, 5_000),
             entersState(connection, VoiceConnectionStatus.Connecting, 5_000),
           ]);
         } catch (error) {
+          console.log(`[GUILD/${interaction.guild.id}] Cannot resume connection to VC "${member.voice.channel.name}", stopping player!`);
+          console.log(`[GUILD/${interaction.guild.id}] Stopped playing (connection lost) in VC "${member.voice.channel.name}"!`);
           connection.destroy();
         }
       });
       const audioPlayer = createAudioPlayer();
       connection.subscribe(audioPlayer) && audioPlayer.play(createAudioResource(songPath));
       interaction.reply({embeds: [{description: `✅ Started Playing in ${member.voice.channel.name}!`, color: `00FF00 `}], ephemeral: true});
+      console.log(`[GUILD/${interaction.guild.id}] Started playing in VC "${member.voice.channel.name}!"`);
       if (loop) {
         audioPlayer.on('stateChange', (oldState, newState) => {
           if (newState.status == `idle`) {
@@ -91,6 +99,7 @@ client.on(`interactionCreate`, async (interaction) => {
       if (member.voice.channel.id == connection.joinConfig.channelId) {
         connection.destroy();
         interaction.reply({embeds: [{description: `✅ Stopped Playing in ${member.voice.channel.name}!`, color: `00FF00 `}], ephemeral: true});
+        console.log(`[GUILD/${interaction.guild.id}] Stopped playing (requested) in VC "${member.voice.channel.name}"!`);
       } else {
         interaction.reply({embeds: [{description: `🛑 I'm not connected to your voice channel!`, color: `ff0000 `}], ephemeral: true});
       };
